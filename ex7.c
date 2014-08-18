0e6bc188c3b9c65b4760f5d03ea6312db8aef032
galatia <g@latia.io>
2014-08-18 13:44:42 -0700

    more exercises, through 7

---
+++ ex7.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+  int bugs = 100;
+  double bug_rate = 1.2;
+
+  long universe_of_defects = 1L * 1024L * 1024L * 1024L;
+  printf("The enire universe 
